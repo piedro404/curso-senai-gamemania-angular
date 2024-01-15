acdb32c2c119107a1fc71eb90ef4e93ac2c568c8
Pedro Henrique <pedroplayborges@gmail.com>
2024-01-15 16:56:49 -0300

    router config

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "inicio", component: InicioComponent }, 
+  { path: "login", component: LoginComponent }, 
+  { path: "", redirectTo: "/inicio", pathMatch: "full" }, 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
