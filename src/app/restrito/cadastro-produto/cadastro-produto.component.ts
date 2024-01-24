62ee9f64b747af76dad606bc745605f17cbca361
Pedro Henrique <pedro.henrique.martins404@gmail.com>
2024-01-24 17:15:05 -0300

    Cadastro Produto Page

--- src/app/restrito/cadastro-produto/cadastro-produto.component.ts
+++ src/app/restrito/cadastro-produto/cadastro-produto.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Produto } from '../../models/Produto.model';
+import { ProdutoService } from '../../produto.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cadastro-produto',
@@ -7,4 +10,21 @@ import { Component } from '@angular/core';
 })
 export class CadastroProdutoComponent {
 
+  public produto: Produto = new Produto(0,"","","",0);
+
+  constructor(private _produtoService: ProdutoService, private _router:Router) {}
+
+  cadastrar(): void {
+    this._produtoService.cadastrarProduto(this.produto).subscribe(
+      produto => {
+        this.produto = new Produto(0,"","","",0);
+        alert("Cadastro Efetuado com Sucesso");
+      },
+      err => {
+        alert("Erro ao Cadastrar");
+      }
+    );
+
+    this._router.navigate(["restrito/lista"]);
+  }
 }
