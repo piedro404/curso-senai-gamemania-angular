2ad17bb835657dcf38a3424e4a3aed9898bb03f0
Pedro Henrique <pedro.henrique.martins404@gmail.com>
2024-01-24 16:49:34 -0300

    Lista Produtos Page

--- src/app/restrito/lista-produto/lista-produto.component.ts
+++ src/app/restrito/lista-produto/lista-produto.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Produto } from '../../models/Produto.model';
+import { ProdutoService } from '../../produto.service';
 
 @Component({
   selector: 'app-lista-produto',
   templateUrl: './lista-produto.component.html',
-  styleUrl: './lista-produto.component.css'
+  styleUrl: './lista-produto.component.css',
 })
-export class ListaProdutoComponent {
+export class ListaProdutoComponent implements OnInit {
+  public produtos: Produto[] = [];
 
+  constructor(private _produtoService: ProdutoService, private router:Router) {}
+
+  ngOnInit(): void {
+    this.listarProdutos();
+  }
+
+  listarProdutos(): void {
+    this._produtoService.getProdutos().subscribe((retornaProduto) => {
+      this.produtos = retornaProduto.map((item) => {
+        return new Produto(
+          item.id,
+          item.produto,
+          item.descricao,
+          item.foto,
+          item.preco
+        );
+      });
+    });
+  }
+
+  excluir(id: number) {
+    this._produtoService.deletarProduto(id).subscribe(
+      (produto) => {
+        this.listarProdutos();
+      },
+      (err) => {
+        console.log('erro ao Excluir');
+      }
+    );
+
+    this.router.navigate(['/restrito/lista']);
+  }
 }
